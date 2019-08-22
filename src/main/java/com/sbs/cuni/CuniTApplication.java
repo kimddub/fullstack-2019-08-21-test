31cc32e0a02068ba139a42c50e126ede22354c72
Subin Kim <kimddub@gmail.com>
2019-08-22 17:49:08 +0900

    first commit

---
+++ src/main/java/com/sbs/cuni/CuniTApplication.java
@@ -0,0 +1,13 @@
+package com.sbs.cuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuniTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuniTApplication.class, args);
+	}
+
+}
